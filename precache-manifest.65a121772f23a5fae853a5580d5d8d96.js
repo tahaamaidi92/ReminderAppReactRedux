b0cd150d9d16e0e674d0fe5c0ec6c3829856e01c
tahaamaidi92 <benhama.taha.33@gmail.com>
2019-10-21 00:05:13 +0100

    Updates

---
+++ precache-manifest.65a121772f23a5fae853a5580d5d8d96.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e60aa4ee26159a7f4dd1f0747bd6a861",
+    "url": "/ReminderAppReactRedux/index.html"
+  },
+  {
+    "revision": "3a25ff376cd222c9d5cb",
+    "url": "/ReminderAppReactRedux/static/css/2.9028a4ae.chunk.css"
+  },
+  {
+    "revision": "f147968763a35a240e5d",
+    "url": "/ReminderAppReactRedux/static/css/main.b47500e6.chunk.css"
+  },
+  {
+    "revision": "3a25ff376cd222c9d5cb",
+    "url": "/ReminderAppReactRedux/static/js/2.c9977b1e.chunk.js"
+  },
+  {
+    "revision": "f147968763a35a240e5d",
+    "url": "/ReminderAppReactRedux/static/js/main.85c09bbd.chunk.js"
+  },
+  {
+    "revision": "797b95e9c2fc1d4e60e0",
+    "url": "/ReminderAppReactRedux/static/js/runtime-main.1d4475de.js"
+  },
+  {
+    "revision": "2c51f81ea0f98a3238ca69bda8990a5f",
+    "url": "/ReminderAppReactRedux/static/media/reminder.2c51f81e.png"
+  }
+]);
